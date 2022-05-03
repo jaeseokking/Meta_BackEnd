b7e7f5ed49334c0ec2f9dbde049bd1c7660b0c11
ovoaaahj <v_v33s@naver.com>
2022-05-03 13:27:42 +0900

    feat: masterController Add 마스터 컨트롤러 추가

---
+++ Back_End/src/main/java/com/real/controller/masterController.java
@@ -0,0 +1,10 @@
+package com.real.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="master")
+public class masterController {
+
+}
