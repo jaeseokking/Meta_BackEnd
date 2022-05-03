58d67d868df52fd1169e013aeb9a5db70659e86a
ovoaaahj <v_v33s@naver.com>
2022-05-03 15:30:20 +0900

    마스터가 admin 추가하는 기능 암호화 했음

--- Back_End/src/main/java/com/real/controller/masterController.java
+++ Back_End/src/main/java/com/real/controller/masterController.java
@@ -2,15 +2,37 @@ package com.real.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.real.dto.MemberVo;
 import com.real.service.masterService;
 
 @Controller
-@RequestMapping(value="master")
+@RequestMapping(value="/master")
 public class masterController {
 	
 	@Autowired
 	private masterService masterservice;
+	
+	/**
+	 * Master가 관리자 넣는 로직
+	 * MemberVo 의 요소와 맞게 잘 넣어야함
+	 * @param member
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value="/AdminA" ,method=RequestMethod.POST )
+	public int AdminA(@RequestBody MemberVo member) {
+		
+		int result = 0;
+		
+		result = masterservice.AdminA(member);
+		
+		return result;
+		
+	}
 
 }
--- Back_End/src/main/java/com/real/dao/masterDao.java
+++ Back_End/src/main/java/com/real/dao/masterDao.java
@@ -4,9 +4,22 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.real.dto.MemberVo;
+
 @Repository
 public class masterDao {
 	@Autowired
 	private SqlSessionTemplate sqlSession;
 	private static String namespace="master_mapper";
+	
+	
+	/**
+	 * master 관리자 추가하기
+	 * @param member
+	 * @return
+	 */
+	public int AdminA(MemberVo member) {
+		// TODO Auto-generated method stub
+		return sqlSession.insert(namespace+".AdminA",member);
+	}
 }
