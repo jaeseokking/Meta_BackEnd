c0ad2e9c83c8bdb12e917657b82421b0d4739c7d
jaeseokking <wotjr294@naver.com>
2022-05-12 13:55:33 +0900

    fix jwt

--- Back_End/src/main/java/com/real/controller/mainController.java
+++ Back_End/src/main/java/com/real/controller/mainController.java
@@ -11,13 +11,17 @@ import java.util.Map.Entry;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.real.dto.MemberVo;
+import com.real.jwt.JwtServiceImpl;
 import com.real.service.mainService;
 
 @Controller
@@ -26,6 +30,43 @@ public class mainController {
 	@Autowired 
 	mainService mainservice;
 
+	@Autowired
+	JwtServiceImpl jwtService;
+	
+	//토큰 있는 사용자 정보를 리턴 
+	@RequestMapping(value="/getUser", method=RequestMethod.GET )
+	public ResponseEntity<Object> getUser(HttpServletRequest request){
+		try {
+			String token = request.getHeader("jwt-auth-token");
+			Map<String, Object> tokenInfoMap = jwtService.getInfo(token);
+			
+			MemberVo member = new ObjectMapper().convertValue(tokenInfoMap.get("member"), MemberVo.class);
+			
+			return new ResponseEntity<Object>(member, HttpStatus.OK);
+		} catch(Exception e) {
+			return new ResponseEntity<Object>(null, HttpStatus.CONFLICT);
+		}
+	}
+
+	@RequestMapping(value="/excludePath/login", method=RequestMethod.POST )
+	public ResponseEntity<Object> login(@RequestBody MemberVo member, HttpServletRequest response) {
+		try {
+			MemberVo DBmember = new MemberVo(); // 원래는 DB에 저장되어 있는 사용자 정보 가져와야 하는 부분
+			DBmember.setId("rhemddj");
+			DBmember.setPassword("12345");
+			
+			if(DBUser.getId().equals(member.getId()) && MemberVo.getPassword().equals(user.getPassword())) { // 유효한 사용자일 경우
+				String token = jwtService.createToken(user); // 사용자 정보로 토큰 생성
+				response.setHeader("jwt-auth-token", token); // client에 token 전달
+				return new ResponseEntity<Object>("login Success", HttpStatus.OK);
+			} else {
+				return new ResponseEntity<Object>("login Fail", HttpStatus.OK);
+			}
+		} catch(Exception e) {
+			return new ResponseEntity<Object>(null, HttpStatus.CONFLICT);
+		}
+	}
+	
 	
 	//로그인 
 	@ResponseBody
